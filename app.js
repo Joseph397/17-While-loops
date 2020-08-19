ed584b972f31accf5317c6fca203c4563bd2a9d5
Joseph397 <gramkracker88@gmail.com>
2020-08-19 13:47:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Loops
+// repeatedly run a block of code while condition is true
+// while Loops
+// TURN OFF AUTOSAVE
+
+let amount = 10;
+
+while(amount > 0) {
+    console.log('I have ' + amount + " dollars and I'm going to the mall");
+    amount--; // without the break the loop will run endlessly browser will crash
+}
